c23bbef4d6531ba9d80cc80c0503dcaba1f45e04
kimSerVer <scd1212@naver.com>
2021-01-12 18:04:55 +0900

    추가 테스트

--- script/main.js
+++ script/main.js
@@ -1,6 +1,9 @@
 onload = function (e) {
   const root = document.querySelector("#root");
   const h1 = document.createElement("h1");
+  const h2 = document.createElement("h2");
   h1.textContent = "h1입니다.";
+  h2.textContent = "h2를 추가했습니다";
   root.appendChild(h1);
+  root.appendChild(h2);
 };
